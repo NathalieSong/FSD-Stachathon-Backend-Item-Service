bfe6372ea05e108d874fd8da94e75d18cdde351a
Jamie <syjsong@cn.ibm.com>
2020-05-13 23:30:04 +0800

    Add unit test

--- src/test/java/com/emart/item/service/itemservice/ItemServiceApplicationTests.java
+++ src/test/java/com/emart/item/service/itemservice/ItemServiceApplicationTests.java
@@ -1,13 +1,63 @@
 package com.emart.item.service.itemservice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import com.emart.item.service.itemservice.dto.CartDto;
+import com.emart.item.service.itemservice.entity.Cart;
+import com.emart.item.service.itemservice.service.CartService;
+import com.emart.item.service.itemservice.vo.CartRMFilter;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
 class ItemServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private CartService cartService;
+
+	private CartDto cartDto;
+	private String cartId;
+
+	@BeforeAll
+	void initCart() {
+		cartDto = new CartDto();
+		cartDto.setBuyerId("1234");
+		cartDto.setItemId("2345");
+		cartDto.setQuantity(2);
+	}
+
+	@Test
+	void cartTest() {
+		CartDto dtoAdded = cartService.addCart(cartDto);
+		assertNotNull(dtoAdded.getId());
+		cartId = dtoAdded.getId();
+		assertEquals(cartDto.getBuyerId(), dtoAdded.getBuyerId());
+		assertEquals(cartDto.getCreatedDate().getTime(), dtoAdded.getCreatedDate().getTime());
+		assertEquals(cartDto.getItemId(), dtoAdded.getItemId());
+		assertEquals(cartDto.getQuantity(), dtoAdded.getQuantity());
+	}
+
+	@AfterAll
+	void rmCartById() {
+		CartRMFilter filter = new CartRMFilter();
+		filter.setBuyerId(cartDto.getBuyerId());
+		filter.setCartIds(Arrays.asList(cartId));
+		cartService.rmCartByIds(filter);
 	}
 
 }
